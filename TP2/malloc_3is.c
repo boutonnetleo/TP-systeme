ce8502409ba94b62d49cff77c81e5af7a8b72500
leo <leo.boutonnet@ensea.fr>
2025-10-07 12:03:58 +0200

    Réutilisation block libéré

--- TP2/malloc_3is.c
+++ TP2/malloc_3is.c
@@ -10,7 +10,7 @@ typedef struct HEADER_TAG
 } HEADER;
 
 HEADER *freelist = NULL;
-int number_overflow=0;
+int number_overflow = 0;
 
 void *malloc_3is(size_t size)
 {
@@ -20,46 +20,53 @@ void *malloc_3is(size_t size)
     {
         if (current->bloc_size >= size)
         {
-
-            HEADER *diff = current + size;
+// Probleme rajoute block vide sic bloc_size == size
+            HEADER *diff = ((void *)current + size);
             diff->bloc_size = current->bloc_size - size;
 
             diff->magic_number = MAGIC_NUMBER;
 
             diff->ptr_next = current->ptr_next;
 
-            previous->ptr_next = diff;
+            if (previous)
+                previous->ptr_next = diff;
+            else
+                freelist->ptr_next = diff;
 
-            return (void *)current + 1;
+            return (void *)((HEADER *)current + 1);
         }
         previous = current;
         current = current->ptr_next;
     }
 
-    if (!current)
+    void *ptr = sbrk(sizeof(HEADER) + size + sizeof(MAGIC_NUMBER));
+    if (ptr == (void *)-1)
     {
-        void *ptr = sbrk(sizeof(HEADER) + size + sizeof(MAGIC_NUMBER));
-        if (ptr == (void *)-1)
-        {
-            return NULL;
-        }
-
-        HEADER *new = (HEADER *)ptr;
-        new->ptr_next = NULL;
-        new->magic_number = MAGIC_NUMBER;
-        new->bloc_size = size;
-        new++;
-        long* magic_ptr = (long*)((void *)new + size);
-        *magic_ptr = MAGIC_NUMBER;
-
-        return (void *)new;
+        return NULL;
     }
-    return NULL;
+
+    HEADER *new = (HEADER *)ptr;
+    new->ptr_next = NULL;
+    new->magic_number = MAGIC_NUMBER;
+    new->bloc_size = size;
+    new++;
+    long *magic_ptr = (long *)((void *)new + size);
+    *magic_ptr = MAGIC_NUMBER;
+
+    return (void *)new;
+}
+int check_overflow(void *ptr)
+{
+    HEADER *new = ptr;
+    new--;
+    size_t size = new->bloc_size;
+    long *magic_ptr = ((void *)ptr + size);
+    return MAGIC_NUMBER != *magic_ptr;
 }
 
 void free_3is(void *ptr)
 {
-    if(check_overflow(ptr)) number_overflow=number_overflow+1;
+    // if(check_overflow(ptr)) number_overflow=number_overflow+1;
 
     HEADER *new = ptr;
     new--;
@@ -76,28 +83,21 @@ void free_3is(void *ptr)
     }
 }
 
-int check_overflow(void *ptr)
-{
-    HEADER *new = ptr;
-    new--;
-    size_t size=new->bloc_size;
-    long* magic_ptr = ((void *)ptr + size);
-    return MAGIC_NUMBER!=*magic_ptr;
-}
-
 int main()
 {
     void *ptr = malloc_3is(4);
-    int *ptr2 =(int*) malloc_3is(3);
+    int *ptr2 = (int *)malloc_3is(3);
     void *ptr3 = malloc_3is(5);
     printf("ptr is %p \r\n", ptr);
     printf("ptr2 is %p \r\n", ptr2);
     printf("ptr3 is %p \r\n", ptr3);
     printf("%d \r\n", freelist != NULL);
     free_3is(ptr);
-    printf("Overflow ptr2 ? %d \r\n",check_overflow(ptr2));
-    *ptr2=NOMBRE_ENORME;
-    printf("Overflow ptr2 ? %d \r\n",check_overflow(ptr2));
+    void *ptr4 = malloc_3is(6);
+    printf("ptr4 is %p \r\n", ptr4);
+    printf("Overflow ptr2 ? %d \r\n", check_overflow(ptr2));
+    *ptr2 = NOMBRE_ENORME;
+    printf("Overflow ptr2 ? %d \r\n", check_overflow(ptr2));
     printf("%d \r\n", freelist != NULL);
     printf("size of ptr was %lx \r\n", freelist->bloc_size);
     printf("%lx \r\n", sizeof(HEADER) + sizeof(MAGIC_NUMBER) + 4);
