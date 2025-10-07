e611180ac5d69a6c8042d8d6208a52e6b2d96a30
leo <leo.boutonnet@ensea.fr>
2025-10-07 10:33:17 +0200

    1ere question TP malloc

--- TP2/malloc_3is.c
+++ TP2/malloc_3is.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 #define MAGIC_NUMBER 0x0123456789ABCDEFL
 
 typedef struct HEADER_TAG
@@ -14,39 +15,41 @@ void *malloc_3is(size_t size)
 {
     HEADER *current = freelist;
     HEADER *previous = NULL;
-    while (current != NULL)
-    {
-        if (current->bloc_size >= size)
-        {
+    // while (current != NULL)
+    // {
+    //     if (current->bloc_size >= size)
+    //     {
 
-            HEADER *diff = current + size;
-            diff->bloc_size = current->bloc_size - size;
+    //         HEADER *diff = current + size;
+    //         diff->bloc_size = current->bloc_size - size;
 
-            diff->magic_number = MAGIC_NUMBER;
+    //         diff->magic_number = MAGIC_NUMBER;
 
-            diff->ptr_next = current->ptr_next;
+    //         diff->ptr_next = current->ptr_next;
 
-            previous->ptr_next = diff;
+    //         previous->ptr_next = diff;
 
-            return (void *)current + 1;
-        }
-        previous = current;
-        current = current->ptr_next;
-    }
+    //         return (void *)current + 1;
+    //     }
+    //     previous = current;
+    //     current = current->ptr_next;
+    // }
 
     if (!current)
     {
         void *ptr = sbrk(sizeof(HEADER) + size + sizeof(MAGIC_NUMBER));
-        if (ptr != (void *)-1)
-            return NULL;
+        if (ptr == (void *)-1){
+            return NULL;}
 
         HEADER *new = (HEADER *)ptr;
         new->ptr_next = NULL;
         new->magic_number = MAGIC_NUMBER;
         new->bloc_size = size;
+        new++;
 
-        return (void*)ptr+1;
+        return (void*)new;
     }
+    return NULL;
 }
 
 
@@ -55,5 +58,9 @@ void *malloc_3is(size_t size)
 
 int main()
 {
-    
+    void* ptr = malloc_3is(4);
+    void* ptr2 = malloc_3is(3);
+    printf("ptr is %p \r\n",ptr);
+    printf("ptr2 is %p \r\n",ptr2);
+    printf("%lx",sizeof(HEADER)+sizeof(MAGIC_NUMBER)+4);
 }
